d9191ba6459f2f6251edf6261e9cf234b0f2f1e3
Pavel Kvasnikov <camokpyt@gmail.com>
2020-04-05 17:54:43 +0300

    WIP

--- lib/endpoint_flux/railtie.rb
+++ lib/endpoint_flux/railtie.rb
@@ -1,5 +1,21 @@
-class EndpointFlux::Railtie < Rails::Railtie
-  rake_tasks do
-    load 'tasks/init.rake'
+require 'endpoint_flux'
+require 'rails'
+
+module EndpointFlux
+  class Railtie < Rails::Railtie
+    railtie_name :endpoint_flux
+
+    rake_tasks do
+      namespace :gem_fresh do
+        desc "outdated"
+        task :outdated => :environment do
+          puts 'sonething'
+        end
+      end
+      #  path = File.expand_path(__dir__)
+      load '/home/pavel/RubymineProjects/endpoint-flux/lib/tasks/init.rake'
+        #   Dir.glob("#{path}/tasks/**/*.rake").each { |f| load f }
+    end
   end
 end
+
